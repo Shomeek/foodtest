9d30be4351a570bccaab0b06a4718b29633b2612
Saktikana Mitra <saktikanamitra@Saktikanas-MacBook-Air.local>
2019-08-21 23:33:18 +0530

    first_commit

---
+++ js.js
@@ -0,0 +1,14 @@
+// Fy first javascript
+console.log('Hello');
+
+// Object
+
+let person = {
+    firstName: 'Shomeek',
+    lastName: 'Basu'
+};
+
+console.log(person.firstName);
+
+
+let name = []
